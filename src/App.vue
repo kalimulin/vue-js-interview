38b5cf37a3fef1f9c581b70f3d98792c15a294d1
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-09-27 16:08:41 +0400

    bild app with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <v-app>
+    <v-main>
+      <ContentBlock />
+    </v-main>
+
+    <v-footer app v-bind="localAttrs">
+      <AppFooter />
+    </v-footer>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import ContentBlock from "./components/ContentBlock.vue";
+import AppFooter from "./components/AppFooter.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    ContentBlock,
+    AppFooter,
   },
+  computed: {
+    localAttrs() {
+      const attrs = {
+        absolute: true,
+        fixed: false,
+      };
+      return attrs;
+    },
+  },
+  data: () => ({
+    //
+  }),
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
